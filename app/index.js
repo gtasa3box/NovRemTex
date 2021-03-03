74cc39857e41a796be4494a42fa2001a31d57128
gtasa3box <gtasa3box@gmail.com>
2021-03-03 19:33:10 +0300

    sliderfix

--- app/index.js
+++ app/index.js
@@ -1,33 +1,5 @@
 import './index.scss';
 
-$(document).ready(function($) {
-	$('.popup-open').click(function() {
-		$('.popup-overlay').addClass('active');
-		$('body').addClass('popup-open-body');
-		return false;
-	});	
-	
-	$('.popup-close').click(function() {
-		$(this).parents('.popup-overlay').removeClass('active');
-		$('body').removeClass('popup-open-body');
-		return false;
-	});		
-
-	$(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-			e.stopPropagation();
-			$('.popup-overlay').removeClass('active');
-		}
-	});
-	
-	$('.popup-overlay').click(function(e) {
-		if ($(e.target).closest('.popup').length == 0) {
-			$(this).removeClass('active');
-			$('body').removeClass('popup-open-body');
-		}
-	});
-
-});
 
 
 $('.js-slick').slick({
